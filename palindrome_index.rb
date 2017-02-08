f6c666d3450f4bb34126ceba29cde465fcb6dc4c
Marlon Silva Carvalho <marlon.carvalho@gmail.com>
2017-02-08 15:23:32 -0300

    Correção de alguns algoritmos

--- palindrome_index.rb
+++ palindrome_index.rb
@@ -1,5 +1,6 @@
 require  'test/unit'
 
+# Funciona mas tem o pior desempenho de todos.
 # def solution(s)
 #   return -1 if s == s.reverse
 #
@@ -12,53 +13,61 @@ require  'test/unit'
 #   -1
 # end
 
-# def solution(s)
-#   return -1 if s == s.reverse
+# Funciona e tem o melhor desempenho.
 #
-#   arr = s.split ''
-#   b = 0
-#   e = s.length - 1
-#   while b != e
-#     if arr[b] != s[e]
-#       temp = arr.dup
-#       temp.delete_at b
-#       if temp == temp.reverse
-#         return b
-#       else
-#         temp = arr.dup
-#         temp.delete_at e
-#         if temp == temp.reverse
-#           return e
-#         end
-#       end
-#     end
-#     b += 1
-#     e -= 1
-#   end
-#
-#   -1
-# end
-
 def solution(s)
-  s = s.split ''
-  r = s.reverse
+  return -1 if s == s.reverse
 
-  for i in (0..s.length - 1)
-    if s[i] != r[i]
-      temp = s.dup
-      temp.delete_at(i)
+  arr = s.split ''
+  b = 0
+  e = s.length - 1
+  while b != e
+    if arr[b] != s[e]
+      temp = arr.dup
+      temp.delete_at b
       if temp == temp.reverse
-        return i
+        return b
       else
-        temp = s.dup
-        temp.delete_at(i + s.length - 1)
-        return i + s.length - 1 if temp == temp.reverse
+        temp = arr.dup
+        temp.delete_at e
+        if temp == temp.reverse
+          return e
+        end
       end
     end
+    b += 1
+    e -= 1
   end
 
   -1
 end
+
+
+# Funciona mas falha em desempenho com strings grandes.
+# O problema é que precisamos de um "for" que percorre toda a string,
+# enquanto a solução acima o "while" percorre metade do caminho do "for"
+# desta solução.
+#
+# def solution(s)
+#   s = s.split ''
+#   r = s.reverse
+#
+#   for i in (0..s.length - 1)
+#     if s[i] != r[i]
+#       temp = s.dup
+#       temp.delete_at(i)
+#       if temp == temp.reverse
+#         return i
+#       else
+#         temp = s.dup
+#         temp.delete_at(i + s.length - 1)
+#         return i + s.length - 1 if temp == temp.reverse
+#       end
+#     end
+#   end
+#
+#   -1
+# end
 
 class Tests < Test::Unit::TestCase
 
